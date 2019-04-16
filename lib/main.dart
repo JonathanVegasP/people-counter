7a13ae1faf695f749975c023974fe7660022cb93
Jonathan Vegas Peixoto <jopxoto12@gmail.com>
2019-04-15 22:32:08 -0300

    Added FlatButtons

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,31 @@ void main() => runApp(MaterialApp(
               fontWeight: FontWeight.bold,
             ),
           ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              FlatButton(
+                child: Text(
+                  "+1",
+                  style: TextStyle(
+                    fontSize: 40.0,
+                    color: Colors.white,
+                  ),
+                ),
+                onPressed: () {},
+              ),
+              FlatButton(
+                child: Text(
+                  "-1",
+                  style: TextStyle(
+                    fontSize: 40.0,
+                    color: Colors.white,
+                  ),
+                ),
+                onPressed: () {},
+              )
+            ],
+          ),
           Text(
             "You may come in!",
             style: TextStyle(
